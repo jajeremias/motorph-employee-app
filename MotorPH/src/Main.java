23ad84d4d611d3da751f1567af8075ae4f8a9a09
nonoy <nonoy@DESKTOP-QNHBL5R>
2023-05-24 09:22:54 +0800

    Add message

--- MotorPH/src/Main.java
+++ MotorPH/src/Main.java
@@ -11,6 +11,8 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
+		System.out.println("Hello Jhon");
+		System.out.println("Hello World");
 	}
 
 }
